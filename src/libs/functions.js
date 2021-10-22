7ceea2278cde7b98a9dd4e448dac288b5de39e1d
GracoTz <gracotz2020@gmail.com>
2021-10-22 16:02:15 +0200

    Archivo json contiene la data de todos los pokemons y el archivo function cambio mucho ya que se remodelo

--- src/libs/functions.js
+++ src/libs/functions.js
@@ -1,21 +1,7 @@
 // Globals Functions
-
-function inputNameValid (req, res, next) {
-    let nums = [0,1,2,3,4,5,6,7,8,9];
-    let name = req.params.name;
-    for (const letter of name) {
-        if (nums.includes(Number(letter))) {
-            res.status(404).send('Introduce a valid name of the pokemon');
-            return false;
-        }
-    }
-    next();
-};
-
-function validIdField (req, res, next) {
-    const { id } = req.params;
-    if (isNaN(Number(id))) res.send('That is not a valid id');
-    else next();
+function validIdField (id) {
+    if (isNaN(Number(id))) return false;
+    else return true;
 }
 
-module.exports = {inputNameValid, validIdField};
+module.exports = {validIdField};
