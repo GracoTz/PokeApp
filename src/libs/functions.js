8e1ae07d8f691fd673f43ded54b0bdcb14e99e62
GracoTz <gracotz2020@gmail.com>
2021-10-22 19:23:21 +0200

    cree una funcion para separar los datos que vienen con comas a un array para que los datos que vengan como types sea un array y no un string y en el file objects quite las propiedades pokemon_id que eran innecesarias

--- src/libs/functions.js
+++ src/libs/functions.js
@@ -1,3 +1,14 @@
 // Globals Functions
 
-module.exports = {};
+// Va a recibir un string y separarlo por comas
+const createArray = (string) => {
+    let res = [];
+    if (string.indexOf(',') !== -1) {
+        res = string.split(',');
+    } else {
+        res[0] = string;
+    }
+    return res;
+}
+
+module.exports = {createArray};
--- src/libs/objects.js
+++ src/libs/objects.js
@@ -6,7 +6,6 @@ const pokemon = {
     Photo: "",
     Description: "",
     Stadistics: {
-        pokemon_id: "",
         HP: 0,
         ATK: 0,
         DEF: 0,
@@ -15,14 +14,12 @@ const pokemon = {
         SPEED: 0
     },
     Basic_Info: {
-        pokemon_id: "",
         Species: "",
         Height: "",
         Weight: "",
         Abilities: ""
     },
     Evolutions: {
-        pokemon_id: "",
         Have_Evolutions: 0,
         Evolutions: "",
         How_Progress: ""
