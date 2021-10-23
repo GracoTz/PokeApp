7606334302d9811cc881ee9f46fd0b506bed58d8
GracoTz <gracotz2020@gmail.com>
2021-10-23 23:02:44 +0200

    Borrado el archivo renders ya que era innesesario jajaja y en favorite lo arregle que no estaba trabajando bien

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -1,30 +1,28 @@
 const express = require('express');
 const Router = express.Router();
 const conn = require('../db');
-const fc = require('../libs/functions');
 
-async function updateFavorite (sql) {
-    let result = await conn.query(sql);
-    res.status(200).json(result);
-}
-
-Router.put('/set-favorite/:id', async (req, res) => {
+Router.get('/set-favorite/:id', async (req, res) => {
     const { id } = req.params;
-    if (fc.validIdField(id)) {
-        let isFavorite = await conn.query(`SELECT favorite FROM pokemons WHERE id=${id}`);
-        console.log(isFavorite);
-        if (isFavorite) updateFavorite(`UPDATE pokemons SET favorite = 0 WHERE id = ${id}`);
-        else updateFavorite(`UPDATE pokemons SET favorite = 1 WHERE id = ${id}`);
+    let isFavorite = await conn.query(`SELECT Favorite FROM pokemons WHERE id=${id}`);
+    if (isFavorite[0]['Favorite']) {
+        await conn.query(`UPDATE pokemons SET Favorite=false WHERE id=${id}`);
+        res.json({ok: true, message: "The pokemon is not favorite now."});
     } else {
-        res.status(404).send({message: 'That is not a number'});
-    } 
+        await conn.query(`UPDATE pokemons SET Favorite=true WHERE id=${id}`);
+        res.json({ok: true, message: "The pokemon is favorite now."});
+    }
+});
+
+Router.get('/isFavorite/:id', async (req, res) => {
+    const { id } = req.params;
+    const sql = `SELECT Favorite From pokemons WHERE id=${id}`;
+    let isFavorite = await conn.query(sql);
+    res.json(isFavorite[0]);
 });
 
 Router.get('/get-favorites', async (req, res) => {
-    let sql = 'SELECT name, type FROM POKEMONS WHERE favorite = 1';
-    let result = await conn.query(sql);
-    console.log(result);
-    res.status(200).json(result);
+    // Obtener todos los pokemones favoritos
 });
 
 module.exports = Router;
